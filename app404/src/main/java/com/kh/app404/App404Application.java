a261c864ac7880c7f445c50ea8666b7b9e6cda22
edeenss <lsew2232@naver.com>
2025-07-25 09:00:25 +0900

    Add files via upload

---
+++ app404/src/main/java/com/kh/app404/App404Application.java
@@ -0,0 +1,13 @@
+package com.kh.app404;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class App404Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(App404Application.class, args);
+	}
+
+}
